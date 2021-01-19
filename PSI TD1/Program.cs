764992cd7fb9c72b412f592ba5138385088ee5b7
William GAINNIER <william.gainnier@edu.devinci.fr>
2021-01-19 17:38:37 +0100

    modif

--- PSI TD1/Program.cs
+++ PSI TD1/Program.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Media;
+using System.ComponentModel;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace PSI_TD1
 {
@@ -12,6 +16,33 @@ namespace PSI_TD1
         static void Main(string[] args)
         {
 
+            
+            Bitmap lena = new Bitmap("lena.bmp");
+
+            /*
+            Bitmap coco1 = new Bitmap("coco.bmp");
+            coco1.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            coco1.Save("cococopie.bmp");
+            */
+
+            /*RectangleF rec = new RectangleF(10.0f, 10.0f, 100.0f, 100.0f);
+            Bitmap l = lena.Clone(rec, PixelFormat.DontCare);
+            l.Save("lenarec.bmp");*/ // A dé commenter pour avoir une sous partie de l'image
+
+            for (int i = 0; i < lena.Height; i++)
+                for (int j = 0; j < lena.Width; j++)
+                {
+                    Color mycolor = lena.GetPixel(i, j);
+                    lena.SetPixel(i, j, Color.FromArgb(255 - mycolor.R, 255 - mycolor.R, 255 - mycolor.R));
+
+                    //   c.SetPixel(i, j, Color.Coral);
+                }
+
+            lena.Save("lenanegative.bmp");
+            Process.Start("lenanegative.bmp");
+
+
+            Console.ReadLine();
         }
     }
 }
