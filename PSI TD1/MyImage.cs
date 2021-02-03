6aaeb47d7373ba9bd585ff1b153805271416ab47
William GAINNIER <william.gainnier@edu.devinci.fr>
2021-02-03 19:37:41 +0100

    Avancement Convert int to endian et endian to int et compréhension  de comment faire la classe MyImage

--- PSI TD1/MyImage.cs
+++ PSI TD1/MyImage.cs
@@ -40,21 +40,35 @@ namespace PSI_TD1
 
         public MyImage(string myfile)
         {
+            this.myfile = myfile;
 
-            byte[] FileByte = File.ReadAllBytes("myfile");
-           
+            byte[] FileByte = File.ReadAllBytes(myfile);   
+
             //myfile est un vecteur composé d'octets représentant les métadonnées et les données de l'image
 
             //Métadonnées du fichier
             Console.WriteLine("\n Header \n");
+
             for (int i = 0; i < 14; i++)
-                Console.Write(FileByte[i] + " ");
+            {
+                myfile += FileByte[i] + " ";
+            }
             //Métadonnées de l'image
             Console.WriteLine("\n HEADER INFO \n");
+
+            myfile = myfile + "\n";
             for (int i = 14; i < 54; i++)
-                Console.Write(FileByte[i] + " ");
+            {
+                myfile += FileByte[i] + " ";
+            }
+            myfile = myfile + "\n";
 
             //mettre header dans un string puis parse dans un tableau 
+
+            int largeur = FileByte[18] + FileByte[19] * 2 ^ 8 + FileByte[20] * 2 ^ 16 + FileByte[21] * 2 ^ 24;
+            int longueur = FileByte[22] + FileByte[23] * 2 ^ 8 + FileByte[24] * 2 ^ 16 + FileByte[25] * 2 ^ 24;
+
+
 
 
             //L'image elle-même
@@ -78,25 +92,62 @@ namespace PSI_TD1
             Console.ReadLine();
 
 
-
-
         }
 
         public void From_image_to_file(string file)
         {
 
+           
+
+
+
         }
+
+        /// <summary>
+        /// permet de convertir une séquence d'octet au format little endian en entier
+        /// </summary>
+        /// <param name="tab"></param>
+        /// <returns></returns>
 
        public int Convertir_endian_to_int(byte[] tab)
         {
+            int endianint = 0;
+           
+            for(int i =0; i< tab.Length; i++ )
+            {
 
+                endianint = endianint + tab[i] * Convert.ToInt32(Math.Pow(256, i));
 
-            return 0;
+            }
+
+            return endianint;
         }
+
+        /// <summary>
+        /// permet de convertir un entier en séquence d'octets au format little endian
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
         
-        public byte[] Convertir_int_to_endian(int val)
+        public byte[] Convertir_int_to_endian(int val, int bit)
         {
-            byte[] tab = new byte[1];
+            int compteur = bit-1;
+            byte[] tab = new byte[bit];
+            byte temp = 0;
+
+            for(int i =0; i<bit; i++)
+            {
+                temp = Convert.ToByte(val / Convert.ToInt32(Math.Pow(256, compteur)));
+                if (temp >= 1)
+                {
+                    val = val - 256 * temp;
+                    tab[i] = temp;
+                }
+                else tab[i] = 0;
+                compteur--;
+
+            }
+            Array.Reverse(tab);
             return tab;
         }
 
